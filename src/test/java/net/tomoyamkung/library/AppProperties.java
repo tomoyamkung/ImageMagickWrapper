08cd340374901e330f7db953cb428df34802aad4
tomoyamkung <tsuyoshi.sugiyama@gmail.com>
2013-12-13 06:45:53 +0900

    テストに必要なリソースとクラスを作成したのでコミット。

---
+++ src/test/java/net/tomoyamkung/library/AppProperties.java
@@ -0,0 +1,31 @@
+package net.tomoyamkung.library;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class AppProperties {
+
+	private static final String APP_PROPERTIES = "app.properties";
+
+	private static AppProperties instance = new AppProperties();
+
+	private Properties prop;
+
+	private AppProperties() {
+		prop = new Properties();
+		try {
+			prop.load(this.getClass().getClassLoader().getResourceAsStream(APP_PROPERTIES));
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static synchronized AppProperties getInstance() {
+		return instance;
+	}
+
+	public String get(String key) {
+		return prop.getProperty(key).trim();
+	}
+	
+}
